e01ae0d8096f806519ab0b38c9bc3893af4539dd
Thanhloc <Nguyenloc12021999vn@gmail.com>
2023-03-26 13:37:59 +0700

    fix

---
+++ js/home.js
@@ -0,0 +1,8 @@
+let spiner = document.getElementById('spin-lucky-well')
+let btn = document.getElementById("spineBtn")
+let number = Math.ceil(Math.random() * 1000);
+btn.onclick = function () {
+    spiner.style.transform = "translate(-50%, -50%) rotate(" + number + "deg)";
+    number += Math.ceil(Math.random() * 1000);
+    console.log(1);
+}
