e3c4ebbcf69d8186f45624d36e57934c97287668
Thanhloc <Nguyenloc12021999vn@gmail.com>
2023-04-22 20:55:24 +0700

    fix

--- js/home.js
+++ js/home.js
@@ -1,18 +1,22 @@
 let spiner = document.getElementById('spin-lucky-well')
 let btn = document.getElementById("spineBtn")
 let footer = document.getElementById("footer")
-let number = Math.ceil(Math.random() * 1000);
+
 let inbox = document.getElementById("inbox")
 let gift_shop = document.getElementById("gift_shop")
 let mission = document.getElementById("mission")
 let history = document.getElementById("history")
 let guide = document.getElementById("guide")
+let imgSpin = document.getElementById("img_spin")
 let img1 = "url(../images/mission_f.png)"
 let img2 = "url(../images/inbox_footer.png)"
 let img3 = "url(../images/giftshop_f.png)"
 let img4 = "url(../images/history_f.png)"
 let img5 = "url(../images/guide_f.png)"
+const cards = document.querySelectorAll('.animation1');
+const cards1 = document.querySelectorAll('.animation2');
 
+let isSpin = false
 document.addEventListener('touchstart', function (e) {
     var content = e.target;
     if (content.scrollTop + content.clientHeight > content.scrollHeight) {
@@ -21,11 +25,29 @@ document.addEventListener('touchstart', function (e) {
 }, { passive: false });
 
 
+let timeOut
 if (btn)
     btn.onclick = function () {
-        spiner.style.transform = "translate(-50%, -50%) rotate(" + number * 5 + "deg)";
-        spiner.style.transition = "3.5s"
-        number = Math.ceil(Math.random() * 1000);
+
+        isSpin = !isSpin
+        clearTimeout(timeOut)
+        let number = Math.ceil(Math.random() * 1000);
+        timeOut = setTimeout(() => {
+            imgSpin.src = "../images/spine.png"
+            isSpin = false
+            clearTimeout(timeOut)
+        }, 3000)
+        if (isSpin) {
+            imgSpin.src = "../images/spine_stop.png"
+            spiner.style.transform = "translate(-50%, -50%) rotate(" + number * 5 + "deg)";
+            spiner.style.transition = "3.5s"
+        }
+        else {
+            imgSpin.src = "../images/spine.png"
+            spiner.style.transform = "translate(-50%, -50%) rotate(" + number + "deg)";
+            spiner.style.transition = "unset"
+
+        }
     }
 // footer.style.background = "url(../images/bg_footer.png)"
 
@@ -47,3 +69,26 @@ guide.onclick = function () {
     return footer.style.background = img5
 
 }
+
+
+let delay = 0; // thời gian độ trễ giữa mỗi thẻ (1000ms = 1s)
+let index = 0;
+let animationDuration = 500; // thời gian animation (3000ms = 3s)
+
+function animateCard() {
+    cards[index].classList.add('shine');
+    cards1[index].classList.add('shine');
+    setTimeout(() => {
+        cards[index].classList.remove('shine');
+        cards1[index].classList.remove('shine');
+        index++;
+        if (index === cards.length) {
+            index = 0;
+        }
+        setTimeout(() => {
+            animateCard();
+        }, delay);
+    }, animationDuration);
+}
+
+animateCard();
